eb60153f32070ee2900235111bb0bccf8bb17176
IreneVarisco <variscoirene@gmail.com>
2022-11-24 10:25:35 +0100

    correzioni e implementazione print da correggere

--- Codici/Algoritmi/RappresentazioneGrafi/RappresentazioneGrafi.go
+++ Codici/Algoritmi/RappresentazioneGrafi/RappresentazioneGrafi.go
@@ -18,7 +18,7 @@ type linkedList struct {
 
 func main() {
 	g := readGrafo(nuovoGrafo())
-	fmt.Println(g)
+	printGrafo(g)
 }
 
 func nuovoGrafo() *grafo {
@@ -29,6 +29,7 @@ func nuovoGrafo() *grafo {
 func readGrafo(g *grafo) *grafo {
 	var isco string
 	// formato n 0:adiacente1,adiacente2,adiacente3 ... n-1:adiacente1,adiacente2,adiacente3
+	// 3 0:1,2 1:2 3:1
 	fmt.Println("inserire il grafo nel formato n 0:adiacente1,adiacente2,adiacente3 1:adiacente1,adiacente2,adiacente3 ... n-1:adiacente1,adiacente2,adiacente3")
 	fmt.Scan(&isco)
 
@@ -45,23 +46,37 @@ func readGrafo(g *grafo) *grafo {
 	for i := 0; i < len(a); i++ {
 		_, err := strconv.Atoi(a[i])
 
-		if err == nil && string(a[i+1]) == ":" {
-			var ll *linkedList
-			for j := i + 2; string(a[j+1]) != " "; j += 2 {
-				ll.val, _ = strconv.Atoi(a[j])
-				ll.next.val, _ = strconv.Atoi(a[j+2])
+		if i != len(a)-1 {
+			if err == nil && string(a[i+1]) == ":" {
+				var ll *linkedList
+				for j := i + 2; string(a[j+1]) != " "; j += 2 {
+					ll.val, _ = strconv.Atoi(a[j])
+					ll.next.val, _ = strconv.Atoi(a[j+2])
+				}
+				g.adiacenti[i] = ll
 			}
-			g.adiacenti[i] = ll
 		}
-
 	}
 	return g
 }
 
-func printGrafo() {
+func printGrafo(g *grafo) {
+	c := 0
+	for _, v := range g.adiacenti {
+		fmt.Print(c, ":")
+		for i := 0; i < len(g.adiacenti); i++ {
+			if v.next != nil {
+				fmt.Print(v.val, ",")
+			} else {
+				fmt.Println(v.val)
+			}
+		}
+		c++
+	}
 
 }
 
 func existsArch(x int, y int) bool {
+	return true
 
 }
